36404211e39f82f7c82a75b9198d8eb76421b581
affan <108389433+affan1002@users.noreply.github.com>
2023-08-16 23:04:14 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -2,7 +2,12 @@
 
 import subprocess
 
+interface = input("interface >")
 
-subprocess.call("ifconfig wlan0 down",shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66",shell=True)
-subprocess.call("ifconfig wlan0 up",shell=True)
+new_mac = input("new mac >")
+print("[+]changing mac address of "+ interface +"to" + new_mac)
+
+
+subprocess.call(["ifconfig",interface,"down"])
+subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
+subprocess.call(["ifconfig",interface,"up"])
